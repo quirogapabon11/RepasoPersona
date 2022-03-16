14bcc5041f1adbe6566bfcbdfd645019f438f7c0
luchoxx87 <neoluis87@gmail.com>
2022-03-16 14:02:27 -0300

    Agregado proyecto de Test

---
+++ RepasoPersona.Core/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RepasoPersona.Core
+{
+    public class Persona
+    {
+        public string Nombre { get; private set; }
+        public string Apellido { get; private set; }
+        public double Efectivo { get; private set; }
+        public Persona() => Efectivo = 0;
+        public Persona(string nombre, string apellido, double efectivo)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Efectivo = efectivo;
+        } 
+
+        public void Debitar(double monto)
+        {
+            if (monto <= 0)
+                throw new ArgumentException("El monto tiene que ser mayor a cero.");
+
+            if (monto > Efectivo)
+                throw new InvalidOperationException("El monto supera al efectivo.");
+            Efectivo -= monto;
+        }
+        public void Acreditar(double monto)
+        {
+            if (monto <= 0)
+                throw new ArgumentException("El monto tiene que ser mayor a cero.");
+            Efectivo += monto;
+        }
+    }
+}
---
+++ RepasoPersona.Test/PersonaTest.cs
@@ -0,0 +1,57 @@
+using System;
+using RepasoPersona.Core;
+using Xunit;
+
+namespace RepasoPersona.Test
+{
+    public class PersonaTest
+    {
+        public Persona Pepito { get; set; }
+
+        public PersonaTest() => Pepito = new Persona("Juan", "Gomez", 0);
+
+        [Fact]
+        public void Constructor()
+        {
+            Assert.Equal("Juan", Pepito.Nombre);
+            Assert.Equal("Gomez", Pepito.Apellido);
+            Assert.Equal(0, Pepito.Efectivo);
+        }
+
+        [Fact]
+        public void AcreditarPositivo()
+        {
+            double esperado = 1000;
+            Pepito.Acreditar(esperado);
+            
+            Assert.Equal(esperado, Pepito.Efectivo, 3);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-157.34)]
+        public void AcreditarCeroONegativo(double monto)
+        {
+            var ex = Assert.Throws<ArgumentException>(() => Pepito.Acreditar(0));
+            Assert.Equal("El monto tiene que ser mayor a cero.", ex.Message);
+        }
+
+        [Fact]
+        public void Debitar()
+        {
+            double monto = 500.45;
+            double debito = 135.45;
+            Pepito.Acreditar(monto);
+            Pepito.Debitar(debito);
+
+            Assert.Equal(monto - debito, Pepito.Efectivo, 2);
+        }
+
+        [Fact]
+        public void DebitarInsuficiente()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => Pepito.Debitar(1000));
+            Assert.Equal("El monto supera al efectivo.", ex.Message);
+        }
+    }
+}
